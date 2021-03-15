b8a4ac6c002b34c34512a718d87319d8b459df6a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-14 17:04:36 -0700

    added the User model

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, redirect, flash, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SECRET_KEY'] = '4bx8a1m3x'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect flask and SQLAlchemy."""
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """Make the users table."""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    first_name = db.Column(db.String(20), nullable = False)
+    last_name = db.Column(db.String(20), unique = True)
+    image_url = db.Column(
+                    db.Text(), 
+                    unique = True, 
+                    default="""https://images.unsplash.com/photo-1508624217470-5ef0f947d8be?ixid=
+                    MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"""
+                    )
+    
+
+    
+
+
