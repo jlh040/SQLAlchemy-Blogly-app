0dab77c278c95def53a0f9e1cf0e075a4d0fb7e6
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-14 17:31:34 -0700

    added a base html template

--- models.py
+++ models.py
@@ -16,11 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     first_name = db.Column(db.String(20), nullable = False)
     last_name = db.Column(db.String(20), unique = True)
-    image_url = db.Column(
-                db.Text(),  
-                default="""https://images.unsplash.com/photo-1508624217470-5ef0f947d8be?ixid=
-                MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"""
-    )
+    image_url = db.Column(db.Text(),  default="https://images.unsplash.com/photo-1508624217470-5ef0f947d8be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80")
 
     
 
